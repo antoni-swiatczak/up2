4e04d7ff06143101e0a661a0c54eef1710ca9fb5
Antoni <antoni.swiatczak@gmail.com>
2025-03-16 19:33:24 +0100

    names

--- PROG_OBIEKTOWE/qt/1_2025-03-02_Z2.py
+++ PROG_OBIEKTOWE/qt/1_2025-03-02_Z2.py
@@ -2,9 +2,16 @@ import sys
 import logging
 logging.basicConfig(level=logging.INFO)
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
-from PySide6.QtGui import QGuiApplication, QFont
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QMessageBox
+from PySide6.QtGui import QGuiApplication, QFont, QPixmap
 from PySide6.QtCore import Qt
+
+class ClickLink(QLabel):
+    def __init__(self, text, parent=None):
+        super().__init__(text, parent)
+    
+    def mousePressEvent(self, event):
+        self.setText("Ładny kwiatek? ->")
 
 class MainWindow(QMainWindow):
 
@@ -18,8 +25,8 @@ class MainWindow(QMainWindow):
         height = (screen_geometry.height() * 0.7)
         self.setMinimumSize(width, height)
 
-        self.label1 = QLabel("Hejka z lewej :)", self)
-        self.label2 = QLabel("Hejka ze środka :)", self)
+        self.label1 = ClickLink("Klinkij ;)", self)
+        self.label2 = QLabel("<a href=\"https://github.com/antoni-swiatczak/up2/blob/main/PROG_OBIEKTOWE/qt/img/1_Z2.jpg\">Hejka ze środka :)</a>", self)
         self.label3 = QLabel("Hejka z prawej :)", self)
 
         self.label1.setGeometry(50, 0, 200, 100)
@@ -30,14 +37,40 @@ class MainWindow(QMainWindow):
         self.label2.setAlignment(Qt.AlignCenter)
         self.label3.setAlignment(Qt.AlignVCenter | Qt.AlignRight)
 
-        font = QFont("Apotos", 13, QFont.Bold)
-        self.label1.setFont(font)
-        self.label2.setFont(font)
-        self.label3.setFont(font)
+        self.label1.setTextFormat(Qt.RichText)
+        self.label2.setTextFormat(Qt.RichText)
+        self.label3.setTextFormat(Qt.RichText)
 
-        self.label1.setStyleSheet("background-color: #222; color: #ddd; padding: 8px;")
-        self.label2.setStyleSheet("background-color: #222; color: #ddd; padding: 8px;")
-        self.label3.setStyleSheet("background-color: #222; color: #ddd; padding: 8px;")
+        self.label1.setFont(QFont("Apotos", 13, QFont.Bold))
+        self.label2.setFont(QFont("Apotos", 13, QFont.Bold))
+        self.label3.setFont(QFont("Apotos", 13, QFont.Bold))
+
+        self.label1.setStyleSheet("color: #ddd; background-color: #222; padding: 8px;")
+        self.label2.setStyleSheet("color: #ddd; background-color: #222; padding: 8px;")
+        self.label3.setStyleSheet("color: #ddd; background-color: #222; padding: 8px;")
+
+        self.label2.setOpenExternalLinks(True)
+        self.label2.setOpenExternalLinks(True)
+
+        self.pixmap1 = QPixmap("./img/1_Z2.jpg")
+        self.pixmap2 = QPixmap("./img/1_Z2.jpg")
+        self.pixmap3 = QPixmap("./img/1_Z2.jpg")
+
+        self.img1 = QLabel(self)
+        self.img2 = QLabel(self)
+        self.img3 = QLabel(self)
+
+        self.img1.setPixmap(self.pixmap1)
+        self.img2.setPixmap(self.pixmap2)
+        self.img3.setPixmap(self.pixmap3)
+
+        self.img1.setGeometry(0, 250, 300, 300)
+        self.img2.setGeometry(350, 250, 400, 400)
+        self.img3.setGeometry(800, 250, 500, 500)
+
+        self.img1.setScaledContents(True)
+        self.img2.setScaledContents(True)
+        self.img3.setScaledContents(True)
 
 
 def main():
@@ -45,7 +78,7 @@ def main():
         app = QApplication(sys.argv)
         window = MainWindow()
         window.show()
-        logging.info("Application started successfully.")
+        logging.info("Status: OK")
         sys.exit(app.exec())
     except Exception as e:
         logging.error(f"An error occurred: {e}")
