d99213ec62e7c9b8429f4d40a4c975a6a47768fe
Antoni Świątczak <antoni.swiatczak@gmail.com>
2025-06-01 14:17:29 +0200

    names

--- ALGORYTMY/7_2025-05-11/Z1_1.py
+++ ALGORYTMY/7_2025-05-11/Z1_1.py
@@ -1,20 +1,28 @@
 def permutacje(ciag):
     
+    # Jeśli ciąg zawiera tylko jeden znak, zwracamy listę z jednym elementem.
     if len(ciag) == 1:
-        return [ciag]
+        return ciag
 
+    # Lista do przechowywania permutacji
     wynik = []
     
+    # Iterujemy po indeksach i literach w ciągu wejściowym
     for i, litera in enumerate(ciag):
+        # Tworzymy nowy ciąg znaków bez aktualnej litery
         pozostale = ""
         for j in range(len(ciag)):
-            if j != i:
+            if j != i:  # Pomijamy literę na pozycji 'i'
                 pozostale += ciag[j]
 
+        # Rekurencyjnie generujemy permutacje dla pozostałych znaków
         for p in permutacje(pozostale):
+            # Dodajemy aktualną literę na początek każdej permutacji
             wynik.append(litera + p)
     
+    # Zwracamy listę wszystkich możliwych permutacji
     return wynik
 
+# Przykładowe użycie funkcji
 ciag = "abc"
-print(permutacje(ciag))
+print(permutacje(ciag))  # Wyświetlenie wszystkich permutacji ciągu "abc"
